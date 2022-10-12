3db49e694a07529461697b1d5412329695e5211c
DedushkaGnom <63311259+DedushkaGnom@users.noreply.github.com>
2022-10-12 16:05:33 +0300

    Инициализация проекта, создание recycler view, подкючение к Api Flickr

---
+++ app/src/main/java/com/example/photogallery/api/FlickrApi.kt
@@ -0,0 +1,16 @@
+package com.example.photogallery.api
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface FlickrApi {
+
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+                "&api_key=7eceabcb232cac6c22871f2427fb6bee" +
+                "&format=json" +
+                "&nojsoncallback = 1" +
+                "&extras=url_s"
+    )
+    fun fetchPhotos(): Call<FlickrResponse>
+}
