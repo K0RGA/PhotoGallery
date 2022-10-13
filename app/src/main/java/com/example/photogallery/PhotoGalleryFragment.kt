50d89c9d3d58be226bd96b3c70b4e01c3f12fdf1
DedushkaGnom <63311259+DedushkaGnom@users.noreply.github.com>
2022-10-13 15:28:08 +0300

    Исправлена ошибка с запросом на сервер

--- app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
@@ -19,26 +19,18 @@ private const val TAG = "PhotoGalleryFragment"
 
 class PhotoGalleryFragment : Fragment() {
 
+    private val photoGalleryViewModel: PhotoGalleryViewModel by viewModels()
     private lateinit var photoRecyclerView: RecyclerView
-    val photoGalleryViewModel: PhotoGalleryViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val flickrLiveData: LiveData<List<GalleryItem>> = FlickrFetchr().fetchPhotos()
-        flickrLiveData.observe(
-            this,
-            Observer { galleryItems ->
-                Log.d(TAG, "Response received: $galleryItems")
-            }
-        )
     }
 
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         val view = inflater.inflate(R.layout.fragment_photo_gallery, container, false)
 
         photoRecyclerView = view.findViewById(R.id.photo_recycler_view)
@@ -51,30 +43,34 @@ class PhotoGalleryFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         photoGalleryViewModel.galleryItemLiveData.observe(
             viewLifecycleOwner,
-            Observer { galleryItem ->
-                photoRecyclerView.adapter = PhotoAdapter(galleryItem)
-            }
-        )
+            Observer { galleryItems ->
+                Log.d(TAG, "Have gallery items from view model $galleryItems")
+                photoRecyclerView.adapter = PhotoAdapter(galleryItems)
+            })
     }
 
-    private class PhotoHolder(itemTextView: TextView) : RecyclerView.ViewHolder(itemTextView) {
+    private class PhotoHolder(itemTextView: TextView)
+        : RecyclerView.ViewHolder(itemTextView) {
+
         val bindTitle: (CharSequence) -> Unit = itemTextView::setText
     }
 
-    private class PhotoAdapter(private val galleryItems: List<GalleryItem>) :
-        RecyclerView.Adapter<PhotoHolder>() {
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoHolder {
+    private class PhotoAdapter(private val galleryItems: List<GalleryItem>)
+        : RecyclerView.Adapter<PhotoHolder>() {
+
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): PhotoHolder {
             val textView = TextView(parent.context)
             return PhotoHolder(textView)
         }
 
+        override fun getItemCount(): Int = galleryItems.size
+
         override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
             val galleryItem = galleryItems[position]
             holder.bindTitle(galleryItem.title)
-        }
-
-        override fun getItemCount(): Int {
-            return galleryItems.size
         }
     }
 
--- app/src/main/java/com/example/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/example/photogallery/api/FlickrApi.kt
@@ -5,12 +5,10 @@ import retrofit2.http.GET
 
 interface FlickrApi {
 
-    @GET(
-        "services/rest/?method=flickr.interestingness.getList" +
-                "&api_key=7eceabcb232cac6c22871f2427fb6bee" +
-                "&format=json" +
-                "&nojsoncallback = 1" +
-                "&extras=url_s"
-    )
+    @GET("services/rest/?method=flickr.interestingness.getList" +
+            "&api_key=7eceabcb232cac6c22871f2427fb6bee" +
+            "&format=json" +
+            "&nojsoncallback=1" +
+            "&extras=url_s")
     fun fetchPhotos(): Call<FlickrResponse>
 }
